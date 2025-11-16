c849016fb21282414a4ba3ae3cdb56c1cfce0e0e
Kameq <radek.rbb@gmail.com>
2025-11-16 18:59:42 +0100

    fix encode function and change FCR to 0

--- encoder.py
+++ encoder.py
@@ -1,10 +1,12 @@
 from gf import GF
-GF = GF(0x11D, 256)
 
 P_X = 0x11D
 ALPHA = 0x02
-N = 255
+Q = 256
+N = Q - 1
 T = 16
+
+GF = GF(P_X, Q)
 
 def poly_multiply(p, g):
     res = [0] * (len(p) + len(g) - 1)
@@ -19,9 +21,8 @@ def poly_multiply(p, g):
 
 def generate_g(t, alpha):
     g = [1]
-    # poprawny zakres 1-33, poniewarz wzór jest dla(x + α^1)(x + α^2)...(x + α^(32))
-    # a nie dla (x + α^0)(x + α^1)...(x + α^(31))
-    for i in range(1, 2 * t + 1):
+    
+    for i in range(0, 2 * t):
         p = [1, GF.pow(alpha, i)]
         g = poly_multiply(p, g)
     return g
@@ -29,17 +30,22 @@ def generate_g(t, alpha):
 G_X = generate_g(T, ALPHA)
 
 def encode(message, g):
-    k = 223
+    k = N - 2 * T
     n_k = N - k
-    if(len(message) > k):
+    msg_len = len(message)
+    
+    if(msg_len > k):
         raise ValueError("Message length must be smaller or equal to 223 bytes")
+    
     encoded_message = message + [0] * n_k
-    for i in range(k):
+    
+    for i in range(msg_len):
         coef = encoded_message[i] # pojedynczy aktualnie  przetwarzany bajt
         if coef != 0:
             for j in range(1, len(g)):
                 encoded_message[i + j] = GF.add(encoded_message[i + j], GF.mul(coef, g[j])) # tworzymy nadmiar 
+    
+    # Przywracamy oryginalną wiadomość (nadpisujemy część z ilorazem)
+    encoded_message[:msg_len] = message
+    
     return encoded_message
-    
-    
-    
